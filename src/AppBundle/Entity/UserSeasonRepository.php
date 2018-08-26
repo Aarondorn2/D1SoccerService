2151a719c1fddf55cbca16ef0622e59529e8cd62
Unknown <Aarondorn2@gmail.com>
2018-08-26 17:33:35 -0400

    season 3

--- src/AppBundle/Entity/UserSeasonRepository.php
+++ src/AppBundle/Entity/UserSeasonRepository.php
@@ -9,7 +9,7 @@ class UserSeasonRepository extends EntityRepository {
     {
         return $this->getEntityManager()
             ->createQuery(
-                'SELECT u FROM AppBundle:UserSeasonEntity u WHERE (u.teamId is null or u.teamId in(999, :teamId)) and u.seasonId = 2'
+                'SELECT u FROM AppBundle:UserSeasonEntity u WHERE (u.teamId is null or u.teamId in(999, :teamId)) and u.seasonId = 3'
             )->setParameter('teamId', $teamId)->getResult();
     }
 
